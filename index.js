ac0ab3ada8aa2222298b6cf3e709cc2c876bdd38
anthonyp1775 <anthony.pino@peopleshores.com>
2026-07-14 17:00:36 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+window.alert("Welcome to random Trivia!")
+window.alert("There are 5 completely random trivia questions in this game.")
+window.alert("True or false. Bees die after they sting you?");
+window.prompt();
+window.alert("The correct answer is True.");
+window.alert("There are 4 questions left.")
+window.alert("What female group sang the Billboard top 100 song Say My Name");
+window.prompt()
+window.alert("The correct answer is Destiny's Child.")
+window.alert("There are 3 questions left.")
+window.alert("How many yards does a football team get penalized for a false start in the NFL?");
+window.prompt()
+window.alert("The correct answer is 5.")
+window.alert("There are 2 questions left.")
+window.alert("What is the name of Thanos's glove that contains special gems in the avengers series");
+window.prompt()
+window.alert("The correct answer is the Infinity Gauntlet.")
+window.alert("There is 1 questions left.")
+window.alert("What is the name of Laurence Fishburne's character in the hit cinema classic The Matrix.");
+window.prompt()
+window.alert("The correct answer is Morpheus.")
+window.alert(`Congratulations you've completed the game! You get Cake - "GLaDos"`)
---
+++ index2.js
@@ -0,0 +1,15 @@
+// const question = "What does a^2 + b^ = in the pythagorean theorum?"
+
+// const answer1 = "c^2"
+
+console.log("do you want to play")
+
+userInput = 'Y'
+
+while(userInput === 'Y'){
+   
+    
+    prompt("do you still want to play")
+
+    userInput = 'N'
+}
